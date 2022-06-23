7e7daf22f73310fb25fcc41ec47d999047c5255f
Jose Felipe <jfbarkokebas@gmail.com>
2022-06-23 15:53:33 -0300

    total, entradas e saidas no display

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
-const transactionUl = document.querySelector('#transactions')
-console.log(transactionUl)
 
+//elementos DOM
+const transactionUl = document.querySelector('#transactions')
+const incomeDisplay = document.querySelector('#money-plus')
+const expenseDisplay = document.querySelector('#money-minus')
+const balanceDisplay = document.querySelector('#balance')
+
+//  mock...
 const dummyTransactions = [
     {id: 1, name: 'bolo de brigadeiro', amount: -20},
     {id: 2, name: 'salario', amount: 300},
@@ -18,14 +23,46 @@ const addTransactionsIntoDOM = transaction =>{
 
     li.classList.add(CSSClass)
     li.innerHTML = `
-        ${transaction.name} <span>${operator} R$ ${amountWithOutOperator}</span><button class="delete-btn">x</button>
+        ${transaction.name} <span>${operator}
+         R$ ${amountWithOutOperator}</span><button class="delete-btn">x</button>
     `
 
     transactionUl.append(li)
 }
 
+//BALANÇO DOS VALORES
+
+    
+    const updateBalanceValues = ()=>{
+        const sum = (accumulator, transaction) => accumulator + transaction
+        //todos os valores:
+        const transactionAmounts = dummyTransactions.map( transaction => transaction.amount)
+        //saldo
+        const total = transactionAmounts.reduce(sum, 0).toFixed(2)
+        
+        //entradas
+        const income = transactionAmounts.filter(value => value > 0).reduce(sum, 0).toFixed(2)
+        
+        //saidas
+        const expense = Math.abs(transactionAmounts
+            .filter(value => value <0)
+            .reduce(sum,0))
+            .toFixed(2)
+        
+
+        //setando no display
+
+        balanceDisplay.textContent = `R$ ${total}`
+        incomeDisplay.textContent = `R$ ${income}`
+        expenseDisplay.textContent = `R$ ${expense}`
+    }
+
+
+//renderizando a tela
+
 const init = () => {
     dummyTransactions.forEach(addTransactionsIntoDOM)
+    updateBalanceValues()
 }
 
 init()
